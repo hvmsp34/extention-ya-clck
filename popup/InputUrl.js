fc09982fef24580491dafa4ed768761ce3eb1e4b
hvmsp34 <mishmark.home@ya.ru>
2026-06-30 10:17:15 +0300

    chore: InputUrl is allocated in a separate file

---
+++ popup/InputUrl.js
@@ -0,0 +1,26 @@
+import { urlValidation, showNotification } from "./utils.js";
+
+const input = urlInput;
+
+export default async function () {
+  const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
+  const { url } = tab;
+  isValidInputUrl(url);
+  input.onclick = function () { this.select(); };
+  input.oninput = function () { isValidInputUrl(this.value); };
+  console.log({ url, tab, input });
+}
+
+function isValidInputUrl(url) {
+  urlValidation(url)
+    .then(isValid => {
+      shortenButton.setAttribute('active', isValid);
+      if (!isValid) throw new Error('Работа с открытой страницей невозможна. Можете ввести URL самостоятельно, адрес должен начинаться с http:// или https://.');
+      input.value = url;
+    })
+    .catch((error) => {
+      showNotification(error.message || error, 'normal', 10000);
+      console.error(error, url);
+    });
+}
+
--- popup/main.js
+++ popup/main.js
@@ -1,10 +1,6 @@
 import UrlHistory from "./History.js";
 import ShortenButton from "./ShortenButton.js";
-import { copyToClipboard, urlValidation, showNotification, isValidInputUrl } from "./utils.js";
+import InputUrl from "./InputUrl.js";
+import { copyToClipboard, urlValidation, showNotification } from "./utils.js";
 
-
-const [tab] = await browser.tabs.query({ active: true, currentWindow: true });
-const { url } = tab;
-isValidInputUrl(url);
-urlInput.onclick = function () { this.select(); };
-urlInput.oninput = function () { isValidInputUrl(this.value); };
+InputUrl();
